44e2ffbc704ff00df19be2ce36203fc413736dbd
clpowe <clpowe@gmail.com>
2024-04-04 06:15:40 -0400

    updated header

--- pages/index.vue
+++ pages/index.vue
@@ -12,20 +12,20 @@
 				:src="Rooster"
 				alt=""
 				height="150"
-				class="left-3/4 md:block h-48 absolute md:left-1/4 md:bottom-12 -z-10 opacity-60"
+				class="left-3/4 md:block h-48 absolute md:h-60 md:left-1/4 md:bottom-12 -z-10 opacity-60"
 			/>
-			<div
-				class="max-w-[40ch] md:max-w-[55ch] justify-start xl:max-w-[75ch] text-slate-700"
-			>
+			<div class="justify-start max-w-[75ch] text-slate-700">
 				<p class="uppercase text-base md:text-xl mb-2">Harmony Rooster LLC</p>
-				<h1
-					class="!leading-[0.9em] text-5xl sm:text-7xl text-wrap md:text-balance uppercase md:text-7xl xl:text-8xl"
-					>Providing
-					<span class="text-primary">Compassionate</span>
+				<h1 class="!leading-[0.9em] text-wrap md:text-balance uppercase"
+					>Providing <br />
+					<span class="text-primary">Compassionate </span>
+					<br />
 					In-Home <span class="text-accent">Care</span></h1
 				>
 			</div>
-			<div class="justify-start md:ml-[50%] flex flex-col gap-4 md:gap-8">
+			<div
+				class="justify-start md:ml-[50%] flex flex-col gap-4 md:gap-8 md:self-end"
+			>
 				<p
 					class="italic text-slate-500 max-w-md text-sm sm:text-sm md:text-xl md:max-w-lg"
 					>We offer compassionate, non-emergency in-home care services designed
@@ -193,10 +193,13 @@
 <style>
 	h1 {
 		line-break: auto;
+		font-size: clamp(2rem, 10vw, 6rem);
 	}
 
 	h2 {
-		@apply uppercase text-4xl xl:text-5xl;
+		font-size: clamp(1.8rem, 5vw, 3rem);
+		line-height: 1em;
+		@apply uppercase;
 
 		span {
 			@apply text-primary;
