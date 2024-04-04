3ce9ed25d94b48217775e1e2337a22fda4bde83c
clpowe <clpowe@gmail.com>
2024-04-03 22:45:29 -0400

    initial setup

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<header>
+			<p>Harmony Rooster</p>
+			<button>Lets Chat</button>
+			<button>813-888-8888</button>
+		</header>
+		<NuxtPage />
+		<footer>
+			<p>Harmony Rooster LLC 2022</p>
+		</footer>
+	</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,145 @@
+<template>
+	<main>
+		<section>
+			<p>Harmony Rooster LLC</p>
+			<h1>Providing Compassionate In-Home Care</h1>
+			<p
+				>We offer compassionate, non-emergency in-home care services designed to
+				promote independence, dignity, and overall well-being.</p
+			>
+			<button>Contact us</button>
+			<button>Free Consoltation</button>
+		</section>
+
+		<section>
+			<h2>Our Services</h2>
+			<p
+				>Harmony Rooster, LLC is here to help. Our team of dedicated and
+				qualified caregivers provides a helping hand with a variety of needs,
+				including</p
+			>
+			<ul>
+				<li>Personal care assistance </li>
+				<li>Meal preparation and light housekeeping</li>
+				<li>Medication reminders and management</li>
+			</ul>
+			<p
+				>We work closely with you and your loved one to create a personalized
+				care plan that promotes self-reliance, personal growth, and a sense of
+				fulfillment.</p
+			>
+		</section>
+
+		<section>
+			<h2>Meet our team</h2>
+			<p
+				>Our caregivers are not just qualified; they are compassionate and
+				dedicated to making a difference in the lives of our clients. They
+				undergo rigorous background checks, training, and ongoing education to
+				ensure they provide the highest quality of care.</p
+			>
+			<ul>
+				<li>
+					<article>
+						<img src="https://picsum.photos/200" alt="" />
+						<h3>John Doe</h3>
+						<p>Caregiver</p>
+						<p
+							>With over 14+ years of experience in the medical field, Derek
+							leads our team with expertise and compassion</p
+						>
+					</article>
+				</li>
+				<li>
+					<article>
+						<img src="https://picsum.photos/200" alt="" />
+						<h3>John Doe</h3>
+						<p>Caregiver</p>
+						<p
+							>With over 14+ years of experience in the medical field, Derek
+							leads our team with expertise and compassion</p
+						>
+					</article>
+				</li>
+			</ul>
+		</section>
+
+		<section>
+			<img src="https://picsum.photos/200" alt="" />
+			<h2>About Us</h2>
+			<p
+				>Harmony Rooster, LLC is a locally owned and operated company based in
+				Tampa, Florida. The company was founded by [Owner Names], who are
+				passionate about providing exceptional in-home care services that
+				empower individuals to live their best lives.</p
+			>
+		</section>
+
+		<section>
+			<h2>Frequently Asked Questions</h2>
+			<p>Find answers to common questions about our services</p>
+			<ul>
+				<li>
+					<h3>How can I get started?</h3>
+					<p
+						>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+						Recusandae, asperiores mollitia, quas aut temporibus tempora
+						eligendi quos consequuntur fuga vel hic dolore expedita, provident
+						officiis architecto soluta impedit! Consectetur, non!</p
+					>
+				</li>
+				<li>
+					<h3>How can I get started?</h3>
+					<p
+						>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+						Recusandae, asperiores mollitia, quas aut temporibus tempora
+						eligendi quos consequuntur fuga vel hic dolore expedita, provident
+						officiis architecto soluta impedit! Consectetur, non!</p
+					>
+				</li>
+				<li>
+					<h3>How can I get started?</h3>
+					<p
+						>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+						Recusandae, asperiores mollitia, quas aut temporibus tempora
+						eligendi quos consequuntur fuga vel hic dolore expedita, provident
+						officiis architecto soluta impedit! Consectetur, non!</p
+					>
+				</li>
+				<li>
+					<h3>How can I get started?</h3>
+					<p
+						>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+						Recusandae, asperiores mollitia, quas aut temporibus tempora
+						eligendi quos consequuntur fuga vel hic dolore expedita, provident
+						officiis architecto soluta impedit! Consectetur, non!</p
+					>
+				</li>
+				<li>
+					<h3>How can I get started?</h3>
+					<p
+						>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+						Recusandae, asperiores mollitia, quas aut temporibus tempora
+						eligendi quos consequuntur fuga vel hic dolore expedita, provident
+						officiis architecto soluta impedit! Consectetur, non!</p
+					>
+				</li>
+			</ul>
+		</section>
+
+		<section>
+			<h2>Contact Us</h2>
+			<p>have a question? We’re here to help!</p>
+
+			<form action="">
+				<label for="name">Name</label>
+				<input type="text" name="name" id="name" />
+				<label for="email">Email</label>
+				<input type="email" name="email" id="email" />
+				<label for="message">Message</label>
+				<textarea name="message" id="message"></textarea>
+				<button type="submit">Submit</button>
+			</form>
+		</section>
+	</main>
+</template>
