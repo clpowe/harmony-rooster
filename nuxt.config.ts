08434eb48ba1d09a8cc738aa6d1e77b9a539acd6
clpowe <clpowe@gmail.com>
2024-11-19 22:16:31 -0500

    added FormKit and zod packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,9 @@ export default defineNuxtConfig({
 		}
 	},
 
-	modules: ['nuxt-svgo', '@nuxt/image', '@nuxt/icon'],
+	modules: ['nuxt-svgo', '@nuxt/image', '@nuxt/icon', '@formkit/nuxt'],
+	formkit: {
+		autoImport: true
+	},
 	compatibilityDate: '2024-11-16'
 })
