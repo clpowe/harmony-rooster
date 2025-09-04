af3482a7fa7c6529f915ee26670fc0855aa02cca
clpowe <clpowe@gmail.com>
2025-09-03 21:49:04 -0400

    updated styles

--- app/components/Courses.vue
+++ app/components/Courses.vue
@@ -35,21 +35,28 @@ function formatDate(dateString: string) {
             <Typography tag="h3" variant="heading-small" >{{course.course_name}}</Typography>
             <Typography tag="p" variant="text">{{course.description}}</Typography>
           </div >
-            <Typography class="content-price" tag="p" variant="text">Price ${{course.cost}}.00</Typography>
+          <div class="content-price">
+            <Typography  tag="p" variant="text-large">Price ${{course.cost}}.00</Typography>
+          </div>
         </div>
         <ul class="sessions-wrapper">
           <li v-for="session in course.sessions" :key="session.id">
             <div class="session_card">
-              <Typography tag="h4" variant="text-large">
-                {{formatDate(session.date) }}
-              </Typography>
-
+              <div class="spots-wrapper">
+                  <p>Available Seats:</p> 
+                  <p class="spots">
+                  {{session.spots_available}}
+                  </p>
+              </div>
               <div>
-                <Typography tag="p" variant="text"><span class="">Time:</span> {{ session.time }}</Typography>
-                <Typography tag="p" variant="text"><span>Spots Remaining:</span> {{session.spots_available}}</Typography>
+                <h4 class="date">
+                  {{formatDate(session.date) }}
+                </h4>
+                <p class="location"><span class=""></span> {{ session.location }}</p>
+                <p class="time"><span class="">Time:</span> {{ session.time }}</p>
               </div>
 
-              <button class="u-btn u-btn--md w-fit u-btn--primary">Book Now</button>
+              <button class="u-btn u-btn--md w-fit u-btn--primary">Register</button>
             </div>
           </li>
         </ul>
@@ -62,4 +69,87 @@ function formatDate(dateString: string) {
 ul {
   list-style: none;
 }
+  .courses {
+    display: grid;
+    gap: var(--space-lg);
+  }
+
+  .courses-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: var(--space-lg);
+  }
+
+  .sessions-wrapper {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+    gap: var(--space-sm);
+  }
+
+  .courses {
+    h2 {
+      margin-bottom: var(--space-xxxs);
+    }
+  }
+
+  .courses-content {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    padding-bottom: var(--space-sm);
+
+    .content-main {
+      display: grid;
+      gap: var(--space-xxs);
+
+      h3 {
+        color: var(--primary-500);
+        text-transform: uppercase;
+      }
+    }
+
+    .content-price {
+      place-self: end;
+      padding: var(--space-xxs) var(--space-xxs);
+      border-radius: var(--radius-lg);
+    }
+  }
+
+  .session_card {
+    display: grid;
+    gap: var(--space-sm);
+
+    padding: var(--space-sm);
+    border: 1px solid #6d6d6d;
+    border-radius: var(--radius-sm);
+
+    .spots-wrapper {
+      display: flex;
+      justify-content: space-between;
+      font-weight: bold;
+      color: var(--primary-500);
+
+      .spots {
+        display: grid;
+        place-items: center;
+        background: var(--primary-500);
+        border-radius: 99999px;
+        color: white;
+        text-align: center;
+        width: 1.75rem;
+        height: 1.75rem;
+      }
+    }
+
+    .date {
+      color: var(--text-1);
+      font-size: 1.25rem;
+      font-weight: bold;
+    }
+    .location {
+      font-size: 0.85rem;
+      opacity: 0.75;
+    }
+  }
+
 </style>
