528c812183282e4c9af85fbb014a138a555d989f
clpowe <clpowe@gmail.com>
2025-09-08 06:47:55 -0400

    more updates to courses

--- app/components/Courses.vue
+++ app/components/Courses.vue
@@ -1,67 +1,100 @@
 <script setup lang="ts">
-const {data} = useAsyncData('courses', () => $fetch('/api/courses'))
-
+const { data } = useAsyncData("courses", () => $fetch("/api/courses"));
 
 function formatDate(dateString: string) {
-    // Use noon UTC so the calendar date is stable in ET regardless of DST
+  // Use noon UTC so the calendar date is stable in ET regardless of DST
   const [year, month, day] = dateString.split("-").map(Number);
-  if(!year || !month || !day) return ''
+  if (!year || !month || !day) return "";
   const utcNoon = new Date(Date.UTC(year, month - 1, day, 12));
   const tz = "America/New_York";
 
-  const dayName   = new Intl.DateTimeFormat("en-US", { weekday: "long", timeZone: tz }).format(utcNoon);
-  const monthName = new Intl.DateTimeFormat("en-US", { month: "short",  timeZone: tz }).format(utcNoon);
-  const dayNumber       = Number(new Intl.DateTimeFormat("en-US", { day: "numeric", timeZone: tz }).format(utcNoon));
+  const dayName = new Intl.DateTimeFormat("en-US", {
+    weekday: "long",
+    timeZone: tz,
+  }).format(utcNoon);
+  const monthName = new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    timeZone: tz,
+  }).format(utcNoon);
+  const dayNumber = Number(
+    new Intl.DateTimeFormat("en-US", { day: "numeric", timeZone: tz }).format(
+      utcNoon,
+    ),
+  );
 
-  const suffix = dayNumber > 3 && dayNumber < 21 ? "th" : (dayNumber % 10 === 1 ? "st" : dayNumber % 10 === 2 ? "nd" : dayNumber % 10 === 3 ? "rd" : "th");
+  const suffix =
+    dayNumber > 3 && dayNumber < 21
+      ? "th"
+      : dayNumber % 10 === 1
+        ? "st"
+        : dayNumber % 10 === 2
+          ? "nd"
+          : dayNumber % 10 === 3
+            ? "rd"
+            : "th";
   return `${dayName} ${monthName} ${dayNumber}${suffix}`;
 }
-
 </script>
-
 
 <template>
   <section class="courses card u-surface-2">
     <div>
       <Typography tag="h2" variant="heading-medium">
-       <span>Courses We</span> <span>Offer</span>
+        <span>Courses We</span> <span>Offer</span>
       </Typography>
-      <Typography tag="p" variant="text">Harmony Rooster, LLC is a locally owned and operated company based in Tampa, Florida. The company was founded by Derek and Cynthia Robinson, who are passionate about providing exceptional in-home care services that empower individuals to live their best lives.</Typography>
+      <Typography tag="p" variant="text">Harmony Rooster, LLC is a locally owned and operated company based in
+        Tampa, Florida. The company was founded by Derek and Cynthia Robinson,
+        who are passionate about providing exceptional in-home care services
+        that empower individuals to live their best lives.</Typography>
     </div>
     <div v-if="data" class="courses-wrapper">
       <div v-for="course in data" :key="course.id">
         <div class="courses-content">
           <div class="content-main">
-            <Typography tag="h3" variant="heading-small" >{{course.course_name}}</Typography>
-            <Typography tag="p" variant="text">{{course.description}}</Typography>
-          </div >
+            <Typography tag="h3" variant="heading-small">{{
+              course.course_name
+              }}</Typography>
+            <Typography tag="p" variant="text">{{
+              course.description
+              }}</Typography>
+          </div>
           <div class="content-price">
-            <Typography  tag="p" variant="text-large">Price ${{course.cost}}.00</Typography>
+            <Typography tag="p" variant="text-large">Price ${{ course.cost }}.00</Typography>
           </div>
         </div>
         <ul class="sessions-wrapper">
           <li v-for="session in course.sessions" :key="session.id">
             <div class="session_card">
-              <div class="spots-wrapper">
-                  <p>Available Seats:</p> 
-                  <p class="spots">
-                  {{session.spots_available}}
-                  </p>
-              </div>
               <div>
-                <h4 class="date">
-                  {{formatDate(session.date) }}
-                </h4>
-                <p class="location"><span class=""></span> {{ session.location }}</p>
-                <p class="time"><span class="">Time:</span> {{ session.time }}</p>
+                <div class="date-wrapper">
+                  <Icon name="lucide:calendar-range" size="1.75em" />
+                  <h4 class="date">
+                    {{ formatDate(session.date) }}
+                  </h4>
+                </div>
               </div>
-
-              <button class="u-btn u-btn--md w-fit u-btn--primary">Register</button>
+              <div class="item-wrapper">
+                <p class="item">
+                  <Icon name="i-lucide-lightbulb" size="1.25em" />
+                  {{ session.location }}
+                </p>
+                <p class="item">
+                  <Icon name="i-lucide-lightbulb" size="1.25em" />
+                  {{ session.time }}
+                </p>
+                <p class="item">
+                  <Icon name="i-lucide-lightbulb" size="1.25em" />{{
+                    session.spots_available
+                  }}
+                  seats available
+                </p>
+              </div>
+              <button class="u-btn u-btn--md u-btn--primary">Register</button>
             </div>
           </li>
         </ul>
       </div>
-    </div> 
+    </div>
   </section>
 </template>
 
@@ -69,97 +102,90 @@ function formatDate(dateString: string) {
 ul {
   list-style: none;
 }
-  .courses {
+
+.courses {
+  display: grid;
+  gap: var(--space-lg);
+}
+
+.courses-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-lg);
+}
+
+.sessions-wrapper {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: var(--space-sm);
+}
+
+.courses {
+  h2 {
+    margin-bottom: var(--space-xxxs);
+  }
+}
+
+.courses-content {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: space-between;
+  padding-bottom: var(--space-sm);
+
+  .content-main {
     display: grid;
-    gap: var(--space-lg);
-  }
+    gap: var(--space-xxs);
 
-  .courses-wrapper {
-    display: flex;
-    flex-direction: column;
-    gap: var(--space-lg);
-  }
-
-  .sessions-wrapper {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
-    gap: var(--space-sm);
-  }
-
-  .courses {
-    h2 {
-      margin-bottom: var(--space-xxxs);
+    h3 {
+      color: var(--primary-500);
+      text-transform: uppercase;
     }
   }
 
-  .courses-content {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    justify-content: space-between;
-    padding-bottom: var(--space-sm);
+  .content-price {
+    place-self: start;
+    padding: var(--space-xxs) var(--space-xxs);
+    border-radius: var(--radius-lg);
+  }
 
-    .content-main {
-      display: grid;
-      gap: var(--space-xxs);
-
-      h3 {
-        color: var(--primary-500);
-        text-transform: uppercase;
-      }
-    }
+  @media (min-width: 768px) {
+    flex-direction: row;
 
     .content-price {
-      place-self: start;
-      padding: var(--space-xxs) var(--space-xxs);
-      border-radius: var(--radius-lg);
-    }
-
-    @media (min-width: 768px) {
-      flex-direction: row;
-
-      .content-price {
-        place-self: end;
-      }
-
+      place-self: end;
     }
   }
+}
 
-  .session_card {
-    display: grid;
-    gap: var(--space-sm);
+.session_card {
+  display: grid;
+  gap: var(--space-sm);
 
-    padding: var(--space-sm);
-    border: 1px solid #6d6d6d;
-    border-radius: var(--radius-sm);
+  padding: var(--space-sm);
+  border: 1px solid var(--neutral-500);
+  border-radius: var(--radius-sm);
 
-    .spots-wrapper {
-      display: flex;
-      justify-content: space-between;
-      font-weight: bold;
-      color: var(--primary-500);
-
-      .spots {
-        display: grid;
-        place-items: center;
-        background: var(--primary-500);
-        border-radius: 99999px;
-        color: white;
-        text-align: center;
-        width: 1.75rem;
-        height: 1.75rem;
-      }
-    }
+  .date-wrapper {
+    display: flex;
+    align-items: center;
+    gap: var(--space-xs);
 
     .date {
       color: var(--text-1);
-      font-size: 1.25rem;
+      font-size: 1.125rem;
       font-weight: bold;
-    }
-    .location {
-      font-size: 0.85rem;
-      opacity: 0.75;
     }
   }
 
+  .item-wrapper {
+    display: grid;
+    gap: var(--space-xxs);
+
+    .item {
+      display: flex;
+      gap: var(--space-xxxs);
+    }
+  }
+}
 </style>
--- app/components/FAQSection.vue
+++ app/components/FAQSection.vue
@@ -1,102 +1,101 @@
 <script setup lang="ts">
-	import {
-		AccordionContent,
-		AccordionHeader,
-		AccordionItem,
-		AccordionRoot,
-		AccordionTrigger
-	} from 'reka-ui'
+import {
+  AccordionContent,
+  AccordionHeader,
+  AccordionItem,
+  AccordionRoot,
+  AccordionTrigger,
+} from "reka-ui";
 
-	const accordionItems = [
-		{
-			value: 'question-1',
-			question: 'What is hame care?',
-			answer: 'Yes. It adheres to the WAI-ARIA design pattern.'
-		},
-		{
-			value: 'question-2',
-			question: 'Who is eligible?',
-			answer:
-				"Yes. It's unstyled by default, giving you freedom over the look and feel."
-		},
-		{
-			value: 'question-3',
-			question: 'How much does it cost?',
-			answer: 'Yes! You can use the transition prop to configure the animation.'
-		},
-		{
-			value: 'question-4',
-			question: 'Is this covered by medicare?',
-			answer: 'Yes! You can use the transition prop to configure the animation.'
-		},
-		{
-			value: 'question-5',
-			question: 'Do you take insurance?',
-			answer: 'Yes! You can use the transition prop to configure the animation.'
-		}
-	]
+const accordionItems = [
+  {
+    value: "question-1",
+    question: "What is hame care?",
+    answer: "Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    value: "question-2",
+    question: "Who is eligible?",
+    answer:
+      "Yes. It's unstyled by default, giving you freedom over the look and feel.",
+  },
+  {
+    value: "question-3",
+    question: "How much does it cost?",
+    answer: "Yes! You can use the transition prop to configure the animation.",
+  },
+  {
+    value: "question-4",
+    question: "Is this covered by medicare?",
+    answer: "Yes! You can use the transition prop to configure the animation.",
+  },
+  {
+    value: "question-5",
+    question: "Do you take insurance?",
+    answer: "Yes! You can use the transition prop to configure the animation.",
+  },
+];
 </script>
 
 <template>
-	<section class="card faq twoCol">
-		<div>
-			<Typography tag="h2" variant="heading-medium"
-				><span>Frequently</span> Asked
-				<span>Questions</span>
-			</Typography>
-			<p>Find answers to common questions about our services</p>
-		</div>
-		<div class="accordion">
-			<template v-for="item in accordionItems" :key="item.value">
-				<div class="detailsColumn">
-					<details>
-						<summary
-							><Typography tag="h3" variant="heading-small">
-								{{ item.question }}
-							</Typography>
-						</summary>
-						<Typography tag="p" variant="text">{{ item.answer }}</Typography>
-					</details>
-				</div>
-			</template>
-		</div>
-	</section>
+  <section class="card faq twoCol">
+    <div>
+      <Typography tag="h2" variant="heading-medium"
+        ><span>Frequently</span> Asked
+        <span>Questions</span>
+      </Typography>
+      <p>Find answers to common questions about our services</p>
+    </div>
+    <div class="accordion">
+      <template v-for="item in accordionItems" :key="item.value">
+        <div class="detailsColumn">
+          <details>
+            <summary>
+              <Typography tag="h3" variant="heading-small">
+                {{ item.question }}
+              </Typography>
+            </summary>
+            <Typography tag="p" variant="text">{{ item.answer }}</Typography>
+          </details>
+        </div>
+      </template>
+    </div>
+  </section>
 </template>
 
 <style scoped>
-	summary {
-		cursor: pointer;
-		padding-block: 1em;
-	}
-	/* Chromium / Firefox */
-	details summary {
-		/* Either remove the list style ... */
-		list-style: none;
+summary {
+  cursor: pointer;
+  padding-block: 1em;
+}
+/* Chromium / Firefox */
+details summary {
+  /* Either remove the list style ... */
+  list-style: none;
 
-		/* ... or change the `display` property to something else */
-		display: flex;
-	}
+  /* ... or change the `display` property to something else */
+  display: flex;
+}
 
-	/* Webkit */
-	details summary::-webkit-details-marker {
-		display: none;
-	}
+/* Webkit */
+details summary::-webkit-details-marker {
+  display: none;
+}
 
-	details::details-content {
-		overflow: hidden;
-		height: 0;
-		border-bottom: 1px solid #333;
-		padding-bottom: 0;
-		/* Enable transitioning of `content-visibility` */
-		transition:
-			padding-bottom 0.3s,
-			height 0.3s,
-			content-visibility 0.3s;
-		transition-behavior: allow-discrete;
-	}
+details::details-content {
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 0;
+  /* Enable transitioning of `content-visibility` */
+  transition:
+    padding-bottom 0.3s,
+    height 0.3s,
+    content-visibility 0.3s;
+  transition-behavior: allow-discrete;
+}
 
-	details[open]::details-content {
-		height: auto;
-		padding-bottom: 1em;
-	}
+details[open]::details-content {
+  height: auto;
+  padding-bottom: 1em;
+}
 </style>
